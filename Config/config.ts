7faeb0b3b5a949c99ff46cfc76ccfc0563934fb3
Evandro Roberto Ropelatto FIlho <evandroropfilho@gmail.com>
2025-02-05 14:40:18 -0300

    Configuração

--- Config/config.ts
+++ Config/config.ts
@@ -1,11 +1,6 @@
-import { Platform } from 'react-native';
-
-// Detecta o ambiente (Android emulador ou dispositivo físico)
-const isAndroidEmulator = Platform.OS === 'android';
-
 export const config = {
-    API_BASE_URL: isAndroidEmulator ? 'http://10.51.3.249:3000' : 'http://10.51.3.249:3000',  // Para o emulador Android, usa 10.0.2.2
-    PROFILE_PICTURES_PATH: '/uploads'
-  };
+  API_BASE_URL: 'http://10.51.3.239:3000',
+  PROFILE_PICTURES_PATH: '/uploads'  // Removed 'Backend' prefix since it should be handled by the server
+};
   
 
